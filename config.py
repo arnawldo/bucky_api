c35675a0da90f9f28055d810cb7d7c97a0f3f2ff
Arnold Taremwa <taremwaarnold@gmail.com>
2017-09-14 22:06:57 +0300

    added flask sqlalchemy ext

--- config.py
+++ config.py
@@ -1,15 +1,21 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'xGA45@f1'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
 
     @staticmethod
     def init_app(app):
         pass
 
+
 class DevelopmentConfig(Config):
     DEBUG = True
+
 
 class TestingConfig(Config):
     TESTING = True
