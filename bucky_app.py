144db0c8979ad925b2e5d4271d06595620d5dff2
Arnold Taremwa <taremwaarnold@gmail.com>
2017-09-15 12:20:00 +0300

    implemented db migration

--- bucky_app.py
+++ bucky_app.py
@@ -1,8 +1,13 @@
 import os
+
+from flask_migrate import Migrate
+
 from bucky import create_app, db
 from bucky.models import User
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
+
 
 @app.shell_context_processor
 def make_shell_context():
