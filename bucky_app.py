9c8503871a71ef48cf6fbe56dd675185e8ea01aa
Arnold Taremwa <taremwaarnold@gmail.com>
2017-09-15 11:55:41 +0300

    created shell context

--- bucky_app.py
+++ bucky_app.py
@@ -1,5 +1,9 @@
 import os
-from bucky import create_app
-
+from bucky import create_app, db
+from bucky.models import User
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(app=app, db=db, User=User)
