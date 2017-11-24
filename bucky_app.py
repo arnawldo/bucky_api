d4932bd50c9a84f507ad82a1a4757128600cb1c0
Arnold Taremwa <taremwaarnold@gmail.com>
2017-11-24 10:02:47 +0300

    [Chore #153115696] Allow CORS

--- bucky_app.py
+++ bucky_app.py
@@ -1,11 +1,13 @@
 import os
 
+from flask_cors import CORS
 from flask_migrate import Migrate
 
 from bucky_api import create_app, db
 from bucky_api.models import User, BucketList, Task
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+CORS(app)
 migrate = Migrate(app, db)
 
 
